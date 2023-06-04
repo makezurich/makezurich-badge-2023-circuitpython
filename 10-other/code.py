3446f4d3a81a1f837f6c5a0d63f1fe08c4d8646b
Yannick Weiss <hey@yannick.dev>
2023-06-04 12:54:17 +0200

    added monotonic time example

--- 10-other/code.py
+++ 10-other/code.py
@@ -1,5 +1,7 @@
 
+###
 # getting cpu ID
+###
 import microcontroller
 import binascii
 print(microcontroller.cpu.uid)
@@ -7,7 +9,16 @@ print(microcontroller.cpu.uid)
 device_uid = binascii.hexlify(microcontroller.cpu.uid).decode("utf-8")
 print(device_uid)
 
-
+###
 # getting CPU temperature in C
+###
 import microcontroller
 print(microcontroller.cpu.temperature)
+
+###
+# getting second counter, which resets to zero on every reset
+# there is also a nanosecond counter time.monotonic_ns()
+###
+print(time.monotonic()) # printing seconds
+time.sleep(1) # sleep 1 second
+print(time.monotonic())
