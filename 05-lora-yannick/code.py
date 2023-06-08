940af8b0ed1f8c726046c5e1338efb78588176b7
Yannick Weiss <hey@yannick.dev>
2023-06-08 21:00:32 +0200

    lora send with payload formatter example

--- 05-lora-yannick/code.py
+++ 05-lora-yannick/code.py
@@ -1,3 +1,7 @@
+# This example packages the data into a bytearray with python struct
+# In ttn_payload_formatter.js we show an example on how to parse it again.
+# You can setup the ttn_payload_formatter.js in the TTN console
+
 # hardware: https://www.seeedstudio.com/LoRa-E5-Wireless-Module-p-4745.html
 # AT Commands https://files.seeedstudio.com/products/317990687/res/LoRa-E5%20AT%20Command%20Specification_V1.0%20.pdf
 # TODO(yw): parse AT command response correctly
@@ -6,6 +10,8 @@ import board
 import busio
 import digitalio
 import time
+import binascii
+import struct
 
 uart = busio.UART(board.GP4, board.GP5, baudrate=9600, timeout=20)
 
@@ -67,22 +73,42 @@ def lora_set_appkey(appkey):
     result_string = at_send(b"AT+KEY=APPKEY," + appkey + "\n", 10)
     return result_string
 
-# TODO(yw): wait for response, with timeout
-def lora_send(msg):
+def lora_send_text(msg):
     """Function to send data, waits for confirmation
     :param msg: data to send, can be string or bytes
     """
     payload = b""
+    downstream_messages = []
+    if isinstance(msg, str):
+        userinput = msg.strip()
+        payload = bytes(userinput, "utf-8")
+    else:
+        print("Please provide a string")
+        return
+
+    cmd = b"AT+CMSG=" + payload + b"\n"
+    response = at_send(cmd, 10)
+    # TODO(yw): check for ": Done"
+    return response
+
+def lora_send_hex(msg):
+    """Function to send data, waits for confirmation
+    :param msg: data to send, can be string or bytes
+    """
+    payload = b""
+    downstream_messages = []
     if isinstance(msg, str):
         userinput = msg.strip()
         payload = bytes(userinput, "utf-8")
     else:
         payload = msg
 
-    cmd = b"AT+CMSG=" + payload + b"\n"
-    result_string = at_send(cmd, 10)
-    # TODO(yw): handle downlink packages, like: +CMSG: PORT: 1; RX: "112233"
-    return result_string
+    payload = binascii.hexlify(payload)
+
+    cmd = b"AT+CMSGHEX=" + payload + b"\n"
+    response = at_send(cmd, 10)
+    # TODO(yw): check for ": Done"
+    return response
 
 print("LoRa IDs:", lora_get_ids())
 
@@ -94,8 +120,41 @@ print("LoRa IDs:", lora_get_ids())
 
 result = lora_join()
 print(result)
-result = lora_send("hello")
-print(result)
 
-while True:
-    pass
+DEVICE_HEALTHY = 0
+DEVICE_LOW_BATTERY = 1
+DEVICE_ERROR = 2
+
+device_state = DEVICE_HEALTHY
+latitude = 47.401330503492595
+longitute = 8.390119229678902
+temperature = 25.4 # °C
+humidity = 25 # %
+co2_ppm = 1240
+
+# struct.pack pattern
+# <: little endian
+# >: big endian
+# b: signed char, 1byte
+# B: unsigned signed char, 1byte 0-255
+# ?: boolean, 1byte 0-1
+# h: short, 2byte
+# i: int, 4byte
+# I: unsigned int, 4byte
+# l: long, 8byte
+# f: float, 4byte
+# d: double, 8byte
+# q: long long int, 8byte
+# Q: unsigned long long, 8byte
+buffer = struct.pack('>bfffbi', device_state, latitude, longitute, temperature, humidity, co2_ppm)
+print("sending", binascii.hexlify(buffer))
+
+response = lora_send_hex(buffer)
+print(response)
+
+# response = lora_send_text("hello world")
+# print(response)
+
+print("finishd")
+
+time.sleep(2)
