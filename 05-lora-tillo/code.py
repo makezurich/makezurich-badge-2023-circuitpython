a5c195d4d54203c8788b649b74cda123c34bee3c
Gonzalo Casas <casas@arch.ethz.ch>
2023-06-10 14:10:55 +0200

    update lora module's `at_send` to wait for an exit condition

--- 05-lora-tillo/code.py
+++ 05-lora-tillo/code.py
@@ -2,7 +2,6 @@ import board
 import busio
 import e5
 
-mode = "OTAA"
 appkey = "your appkey"
 appeui = "your appeiu"
 
@@ -12,6 +11,6 @@ uart = busio.UART(board.GP4, board.GP5, baudrate=9600)
 lora_module = e5.LoRaModule(uart)
 
 lora_module.show_information()
-lora_module.setup_and_join(mode, appkey, appeui)
+lora_module.setup_and_join_otaa(appkey, appeui)
 
-lora_module.send("MSGHEX", "01234567")
+lora_module.send_hex("01234567")
--- 12-mz_badge_example/lorawan_simple.py
+++ 12-mz_badge_example/lorawan_simple.py
@@ -1,9 +1,10 @@
 import mz_badge
-import os
+
+appkey = "YOUR_APP_KEY_HERE, eg. FFFFFFFFFFFFFFFFFF"
+appeui = "YOUR_APPEUI_HERE, eg. AAAAAAAAAAAA"
 
 lora_module = mz_badge.LoRaModule()
 lora_module.show_information()
 
-# lora_module.setup_and_join_otaa(os.getenv(mode), os.getenv(appkey), os.getenv(appeui))
-lora_module.join()
+lora_module.setup_and_join_otaa(appkey, appeui)
 lora_module.send_string("01234567")
